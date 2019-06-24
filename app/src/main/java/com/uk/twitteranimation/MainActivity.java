1b723d9ba2cd08f5085d9b59611137c06c10d110
usman18 <secretdeveloper18@gmail.com>
2019-06-25 00:24:21 +0530

    ->Rotating smoothly so far.

--- app/src/main/java/com/uk/twitteranimation/MainActivity.java
+++ app/src/main/java/com/uk/twitteranimation/MainActivity.java
@@ -1,13 +1,33 @@
 package com.uk.twitteranimation;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+	
+	private FloatingActionButton fab;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		fab = findViewById(R.id.floatingActionButton);
+		
+		findViewById(R.id.tvAnimate)
+			.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					
+					fab.animate()
+						.rotationBy(360)
+						.setDuration(500)
+						.start();
+					
+				}
+			});
 	}
 }
