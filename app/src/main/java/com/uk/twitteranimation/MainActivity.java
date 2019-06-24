c76efb2bb59fe8e3de7753b0e9925bc82b440932
usman18 <secretdeveloper18@gmail.com>
2019-06-25 00:59:47 +0530

    -> Animation completed.

--- app/src/main/java/com/uk/twitteranimation/MainActivity.java
+++ app/src/main/java/com/uk/twitteranimation/MainActivity.java
@@ -4,10 +4,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
 
 public class MainActivity extends AppCompatActivity {
 	
 	private FloatingActionButton fab;
+	
+	private int drawableId = R.drawable.feather;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +20,36 @@ public class MainActivity extends AppCompatActivity {
 		
 		fab = findViewById(R.id.floatingActionButton);
 		
-		findViewById(R.id.tvAnimate)
+		findViewById(R.id.rootLayout)
 			.setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View v) {
 					
+					fab.setImageResource(drawableId);
+					
+					
 					fab.animate()
-						.rotationBy(360)
-						.setDuration(500)
+						.rotationBy(180)
+						.setDuration(180)
+						.scaleX(1.2f)           //Scaling to 120%
+						.scaleY(1.2f)
+						.withEndAction(new Runnable() {
+							@Override
+							public void run() {
+								
+								fab.animate()
+									.rotationBy(180)
+									.setDuration(180)
+									.scaleX(1)              //Scaling back to what it was
+									.scaleY(1).start();
+								
+								
+								drawableId = (drawableId == R.drawable.feather) ? R.drawable.ic_mail_outline : R.drawable.feather;
+							}
+						})
 						.start();
 					
+					//Changing drawable Id
 				}
 			});
 	}
