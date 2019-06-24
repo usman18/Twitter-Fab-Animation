623a341bfdab0e7b89e0d569c47be7366c720945
usman18 <secretdeveloper18@gmail.com>
2019-06-25 01:05:04 +0530

    -> Finishes done.

--- app/src/main/java/com/uk/twitteranimation/MainActivity.java
+++ app/src/main/java/com/uk/twitteranimation/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 		
 		fab = findViewById(R.id.floatingActionButton);
 		
-		findViewById(R.id.rootLayout)
+		findViewById(R.id.button)
 			.setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View v) {
